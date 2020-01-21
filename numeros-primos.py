b6227897fae76166d5a0e4764e005affe0b44155
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-01-20 21:14:46 -0600

    Calcular si un número es primo con Python

---
+++ numeros-primos.py
@@ -0,0 +1,28 @@
+# -*- coding= UTF-8 -*-
+
+
+def is_primo(number):
+    if number < 2:
+        return False
+    elif  number == 2:
+        return True
+    elif number > 2 and number % 2 == 0:
+        return False
+    else:
+        for i in range(3, number):
+            if number % i == 0:
+                return False
+           
+    return True
+
+def run():
+    number = int(input('Escribe un número: '))
+    result = is_primo(number)
+
+    if result is True:
+        print('Tu número es primo')
+    else:
+        print('Tu numero no es primo')
+
+if __name__ == '__main__':
+    run()
