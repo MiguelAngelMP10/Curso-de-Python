ad241815519d338ffed989fc58f6e1fddd5d456c
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-05-28 23:52:10 -0500

    Lógica del ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -2,6 +2,27 @@
 import random
 
 
+mensajeFelicidads = '''
+oooooooooooo           oooo   o8o             o8o        .o8                  .o8
+`888'     `8           `888   `"'             `"'       "888                 "888
+888          .ooooo.   888  oooo   .ooooo.  oooo   .oooo888   .oooo.    .oooo888   .ooooo.   .oooo.o
+888oooo8    d88' `88b  888  `888  d88' `"Y8 `888  d88' `888  `P  )88b  d88' `888  d88' `88b d88(  "8
+888    "    888ooo888  888   888  888        888  888   888   .oP"888  888   888  888ooo888 `"Y88b.
+888         888    .o  888   888  888   .o8  888  888   888  d8(888  888   888  888    .o o.)88b
+o888o        `Y8bod8P' o888o o888o `Y8bod8P' o888o `Y8bod88P" `Y888""8o `Y8bod88P" `Y8bod8P' 8""888P'
+'''
+
+mensajeGameOver = '''
+ #####                                                      
+#     #   ##   #    # ######     ####  #    # ###### #####  
+#        #  #  ##  ## #         #    # #    # #      #    # 
+#  #### #    # # ## # #####     #    # #    # #####  #    # 
+#     # ###### #    # #         #    # #    # #      #####  
+#     # #    # #    # #         #    #  #  #  #      #   #  
+ #####  #    # #    # ######     ####    ##   ###### #    # 
+'''
+
+
 IMAGES = ['''
 
     +---+
@@ -100,6 +121,33 @@ def run():
     while True:
         display_board(hidden_word, tries)
         current_letter = str(input('Escoge una letra: '))
+
+        letter_indexes = []
+        for idx in range(len(word)):
+            if word[idx] == current_letter:
+                letter_indexes.append(idx)
+
+        if len(letter_indexes) == 0:
+            tries += 1
+
+            if tries == 7:
+                display_board(hidden_word, tries)
+                print('')
+                print(mensajeGameOver)
+                print('¡Perdiste! La palabra correcta era {}'.format(word))
+                break
+
+        else:
+            for idx in letter_indexes:
+                hidden_word[idx] = current_letter
+
+            letter_indexes = []
+
+        try:
+            hidden_word.index('_')
+        except ValueError:
+            print(mensajeFelicidads)
+            print('La palabra es =>{}'.format(word))
 
 
 if __name__ == '__main__':
