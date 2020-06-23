ad753554c52d675c36a653767913c0644d7a30d0
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-06-23 15:39:12 -0500

    Listar y eliminar contctactos

--- servidorAppEngine/main.py
+++ servidorAppEngine/main.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
 from google.cloud import ndb
 from contact_model import Contact
 app = Flask(__name__)
-
+app.secret_key = 'some_secret'
+app.debug = True
 
 @app.route(r'/', methods=['GET'])
 def contac_book():
-    return render_template('contact_book.html')
+    contacts = Contact.query().fetch()
+    return render_template('contact_book.html', contacts=contacts)
 
 
 @app.route(r'/add', methods=['GET', 'POST'])
@@ -16,7 +18,23 @@ def add_comtact():
                           phone=request.form.get('phone'),
                           email=request.form.get('email'))
         contact.put()
+        flash('¡Se añadió el contacto!')
     return render_template('add_contact.html')
+
+@app.route(r'/contacts/<uid>', methods=['GET'])
+def contact_details(uid):
+    contact = Contact.get_by_id(int(uid))
+
+    if not contact:
+        return redirect('/', code=301)
+
+    return render_template('contact.html', contact=contact)
+
+@app.route(r'/delete', methods=['POST'])
+def delete_contact():
+    contact = Contact.get_by_id(int(request.form.get('uid')))
+    contact.key.delete()
+    return redirect('/contacts/{}'.format(contact.key.id()))
 
 
 if __name__ == '__main__':
